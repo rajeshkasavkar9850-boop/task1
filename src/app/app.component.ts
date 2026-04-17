6c559218481b9e8bea03498f7a3bc291ed6b63ca
rajeshkasavkar9850-boop <rajeshkasavkar9850@gmail.com>
2026-04-17 15:33:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Basics';
+export class AppComponent { 
+  title1 = 'Skills';
+  skills : Array<string> = ['HTML','JS','CSS','TS','Angular'];
+
+title2 = 'Programming Languages';
+languages: Array<string> = ['JavaScript','TypeScript','Python','Java','C++']
+
+title3 = 'Hobbies';
+hobbies: Array<string> = ['Reading','Gaming','Traveling','Music','Photography'];
+
+title4 = 'Frameworks';
+frameworks: Array<string> = ['Angular','React','Vue','Node.js','Express'];
+
+title5 = 'Soft Skills';
+softSkills: Array<string> = ['Communication','Teamwork','Problem Solving','Time Management','Leadership'];
+
+title6 = 'Daily Tasks';
+tasks: Array<string> = ['Wake up early','Exercise','Study Angular','Practice Coding','Sleep on time'];
+
 }
